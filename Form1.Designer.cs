2f7fe0213c1a509fea09638a1e68637abd1ecb16
Unknown Coder <ksy@vs21.kr>
2022-09-13 15:13:40 +0900

    Debug Consol Show & Task Test

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -45,70 +45,77 @@ namespace Task_Test_Console
             // 
             // btn_RandomStart
             // 
-            this.btn_RandomStart.Location = new System.Drawing.Point(859, 103);
+            this.btn_RandomStart.Location = new System.Drawing.Point(680, 54);
             this.btn_RandomStart.Name = "btn_RandomStart";
             this.btn_RandomStart.Size = new System.Drawing.Size(307, 23);
             this.btn_RandomStart.TabIndex = 5;
             this.btn_RandomStart.Text = "Random : Task 시작";
             this.btn_RandomStart.UseVisualStyleBackColor = true;
+            this.btn_RandomStart.Click += new System.EventHandler(this.btn_RandomStart_Click);
             // 
             // btn_RandomStop
             // 
-            this.btn_RandomStop.Location = new System.Drawing.Point(859, 132);
+            this.btn_RandomStop.Location = new System.Drawing.Point(680, 83);
             this.btn_RandomStop.Name = "btn_RandomStop";
             this.btn_RandomStop.Size = new System.Drawing.Size(307, 23);
             this.btn_RandomStop.TabIndex = 6;
             this.btn_RandomStop.Text = "Random : Task 정지";
             this.btn_RandomStop.UseVisualStyleBackColor = true;
+            this.btn_RandomStop.Click += new System.EventHandler(this.btn_RandomStop_Click);
             // 
             // btn_NumberStart
             // 
-            this.btn_NumberStart.Location = new System.Drawing.Point(533, 103);
+            this.btn_NumberStart.Location = new System.Drawing.Point(354, 54);
             this.btn_NumberStart.Name = "btn_NumberStart";
             this.btn_NumberStart.Size = new System.Drawing.Size(307, 23);
             this.btn_NumberStart.TabIndex = 7;
             this.btn_NumberStart.Text = "Number Count : Task 시작";
             this.btn_NumberStart.UseVisualStyleBackColor = true;
+            this.btn_NumberStart.Click += new System.EventHandler(this.btn_NumberStart_Click);
             // 
             // btn_RandomRun
             // 
-            this.btn_RandomRun.Location = new System.Drawing.Point(859, 72);
+            this.btn_RandomRun.Location = new System.Drawing.Point(680, 23);
             this.btn_RandomRun.Name = "btn_RandomRun";
             this.btn_RandomRun.Size = new System.Drawing.Size(307, 23);
             this.btn_RandomRun.TabIndex = 8;
             this.btn_RandomRun.Text = "Random : Task Run";
             this.btn_RandomRun.UseVisualStyleBackColor = true;
+            this.btn_RandomRun.Click += new System.EventHandler(this.btn_RandomRun_Click);
             // 
             // btn_NumberStop
             // 
-            this.btn_NumberStop.Location = new System.Drawing.Point(533, 132);
+            this.btn_NumberStop.Location = new System.Drawing.Point(354, 83);
             this.btn_NumberStop.Name = "btn_NumberStop";
             this.btn_NumberStop.Size = new System.Drawing.Size(307, 23);
             this.btn_NumberStop.TabIndex = 9;
             this.btn_NumberStop.Text = "Number Count : Task 정지";
             this.btn_NumberStop.UseVisualStyleBackColor = true;
+            this.btn_NumberStop.Click += new System.EventHandler(this.btn_NumberStop_Click);
             // 
             // btn_NumberRun
             // 
-            this.btn_NumberRun.Location = new System.Drawing.Point(533, 71);
+            this.btn_NumberRun.Location = new System.Drawing.Point(354, 22);
             this.btn_NumberRun.Name = "btn_NumberRun";
             this.btn_NumberRun.Size = new System.Drawing.Size(307, 23);
             this.btn_NumberRun.TabIndex = 10;
             this.btn_NumberRun.Text = "Number Count : Task Run";
             this.btn_NumberRun.UseVisualStyleBackColor = true;
+            this.btn_NumberRun.Click += new System.EventHandler(this.btn_NumberRun_Click);
             // 
             // btn_TaskStart
             // 
-            this.btn_TaskStart.Location = new System.Drawing.Point(200, 103);
+            this.btn_TaskStart.Location = new System.Drawing.Point(21, 54);
             this.btn_TaskStart.Name = "btn_TaskStart";
             this.btn_TaskStart.Size = new System.Drawing.Size(307, 23);
             this.btn_TaskStart.TabIndex = 11;
             this.btn_TaskStart.Text = "시간 : Task 시작";
             this.btn_TaskStart.UseVisualStyleBackColor = true;
+            this.btn_TaskStart.Click += new System.EventHandler(this.btn_TaskStart_Click);
             // 
             // txtBox_RandomTask
             // 
-            this.txtBox_RandomTask.Location = new System.Drawing.Point(859, 161);
+            this.txtBox_RandomTask.Location = new System.Drawing.Point(680, 112);
             this.txtBox_RandomTask.Name = "txtBox_RandomTask";
             this.txtBox_RandomTask.Size = new System.Drawing.Size(307, 600);
             this.txtBox_RandomTask.TabIndex = 2;
@@ -116,25 +123,27 @@ namespace Task_Test_Console
             // 
             // btn_TaskStop
             // 
-            this.btn_TaskStop.Location = new System.Drawing.Point(200, 132);
+            this.btn_TaskStop.Location = new System.Drawing.Point(21, 83);
             this.btn_TaskStop.Name = "btn_TaskStop";
             this.btn_TaskStop.Size = new System.Drawing.Size(307, 23);
             this.btn_TaskStop.TabIndex = 12;
             this.btn_TaskStop.Text = "시간 : Task 정지";
             this.btn_TaskStop.UseVisualStyleBackColor = true;
+            this.btn_TaskStop.Click += new System.EventHandler(this.btn_TaskStop_Click);
             // 
             // btn_TimeRun
             // 
-            this.btn_TimeRun.Location = new System.Drawing.Point(200, 71);
+            this.btn_TimeRun.Location = new System.Drawing.Point(21, 22);
             this.btn_TimeRun.Name = "btn_TimeRun";
             this.btn_TimeRun.Size = new System.Drawing.Size(307, 23);
             this.btn_TimeRun.TabIndex = 13;
             this.btn_TimeRun.Text = "시간 : Task Run";
             this.btn_TimeRun.UseVisualStyleBackColor = true;
+            this.btn_TimeRun.Click += new System.EventHandler(this.btn_TimeRun_Click);
             // 
             // txtBox_NumberTask
             // 
-            this.txtBox_NumberTask.Location = new System.Drawing.Point(533, 161);
+            this.txtBox_NumberTask.Location = new System.Drawing.Point(354, 112);
             this.txtBox_NumberTask.Name = "txtBox_NumberTask";
             this.txtBox_NumberTask.Size = new System.Drawing.Size(307, 600);
             this.txtBox_NumberTask.TabIndex = 3;
@@ -142,7 +151,7 @@ namespace Task_Test_Console
             // 
             // txtBox_TimeTask
             // 
-            this.txtBox_TimeTask.Location = new System.Drawing.Point(200, 161);
+            this.txtBox_TimeTask.Location = new System.Drawing.Point(21, 112);
             this.txtBox_TimeTask.Name = "txtBox_TimeTask";
             this.txtBox_TimeTask.Size = new System.Drawing.Size(307, 600);
             this.txtBox_TimeTask.TabIndex = 4;
@@ -152,7 +161,7 @@ namespace Task_Test_Console
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1366, 833);
+            this.ClientSize = new System.Drawing.Size(1008, 729);
             this.Controls.Add(this.btn_RandomStart);
             this.Controls.Add(this.btn_RandomStop);
             this.Controls.Add(this.btn_NumberStart);
--- Form1.cs
+++ Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,10 +13,149 @@ namespace Task_Test_Console
 {
     public partial class Form1 : Form
     {
+
+        public class Win32
+        {
+            [DllImport("kernel32.dll")]
+            public static extern Boolean AllocConsole();
+
+            [DllImport("kernel32.dll")]
+            public static extern Boolean FreeConsole();
+        }
+
         public Form1()
         {
             InitializeComponent();
+
+#if DEBUG
+            Win32.AllocConsole();
+#else
+            Win32.FreeConsole();
+#endif
         }
 
+        private bool isTimeTask = true;
+
+        private void btn_TimeRun_Click(object sender, EventArgs e)
+        {
+            var TimeTask = Task.Run(() =>
+            {
+                while (isTimeTask)
+                {
+                    txtBox_TimeTask.Invoke(new Action(() =>
+                    {
+                        txtBox_TimeTask.AppendText($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ff")}");
+                        txtBox_TimeTask.AppendText(Environment.NewLine);
+                        txtBox_TimeTask.ScrollToCaret();
+                    }));
+
+                    Console.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss ff")}");
+
+                    Task.Delay(1000).Wait();
+                }
+            });
+        }
+
+        private void btn_TaskStart_Click(object sender, EventArgs e)
+        {
+            if (!isTimeTask)
+            {
+                isTimeTask = true;
+                btn_TimeRun_Click(this, null);
+            }
+        }
+
+        private void btn_TaskStop_Click(object sender, EventArgs e)
+        {
+            if (isTimeTask)
+            {
+                isTimeTask = false;
+            }
+        }
+
+        private bool isNumberTask = true;
+
+        private void btn_NumberRun_Click(object sender, EventArgs e)
+        {
+            var NumberTask = Task.Run(() =>
+            {
+                int nCount = 1;
+
+                while (isNumberTask)
+                {
+                    txtBox_NumberTask.Invoke(new Action(() =>
+                    {
+                        txtBox_NumberTask.AppendText($"{nCount}");
+                        txtBox_NumberTask.AppendText(Environment.NewLine);
+                        txtBox_NumberTask.ScrollToCaret();
+                    }));
+
+                    Console.WriteLine($"{nCount}");
+
+                    nCount++;
+
+                    Task.Delay(10).Wait();
+                }
+            });
+        }
+
+        private void btn_NumberStart_Click(object sender, EventArgs e)
+        {
+            if (!isNumberTask)
+            {
+                isNumberTask = true;
+                btn_NumberRun_Click(this, null);
+            }
+        }
+
+        private void btn_NumberStop_Click(object sender, EventArgs e)
+        {
+            if (isNumberTask)
+            {
+                isNumberTask = false;
+            }
+        }
+
+        private bool isRandomTask = true;
+
+        private void btn_RandomRun_Click(object sender, EventArgs e)
+        {
+            var RandomTask = Task.Run(() =>
+            {
+                while (isRandomTask)
+                {
+                    Random ran = new Random(DateTime.Now.Millisecond + DateTime.Now.Minute + DateTime.Now.Hour);
+
+                    var vRandom = ran.Next(100);
+
+                    txtBox_RandomTask.Invoke(new Action(() => {
+                        txtBox_RandomTask.AppendText($"{vRandom}");
+                        txtBox_RandomTask.AppendText(Environment.NewLine);
+                        txtBox_RandomTask.ScrollToCaret();
+                    }));
+
+                    Console.WriteLine($"{vRandom}");
+
+                    Task.Delay(10).Wait();
+                }
+            });
+        }
+
+        private void btn_RandomStart_Click(object sender, EventArgs e)
+        {
+            if (!isRandomTask)
+            {
+                isRandomTask = true;
+                btn_RandomRun_Click(this, null);
+            }
+        }
+
+        private void btn_RandomStop_Click(object sender, EventArgs e)
+        {
+            if (isRandomTask)
+            {
+                isRandomTask = false;
+            }
+        }
     }
 }
